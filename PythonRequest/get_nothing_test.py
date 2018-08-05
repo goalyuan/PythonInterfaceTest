ab742d2af23edab477282d8a29e680c6483e0d49
gaoyuan <goalyuan@163.com>
2018-08-05 11:15:48 +0800

    Initial commit

---
+++ PythonRequest/get_nothing_test.py
@@ -0,0 +1,11 @@
+import requests
+
+host = "https://httpbin.org/"
+endpoint = "get"
+url = "".join([host, endpoint])
+# print(url)
+
+r = requests.get(url)
+print(r.url)#获取url
+print(r.status_code,r.reason)#获取状态码
+print(r.headers)
